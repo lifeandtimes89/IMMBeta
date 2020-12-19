7ce3d0bfa99d4b49b079ee5a58bf1651124fcae8
lifeandtimes89 <60407112+lifeandtimes89@users.noreply.github.com>
2020-12-19 16:48:58 +0000

    Add script to make  balls dissapear on impact

---
+++ Assets/3rdPerson+Fly/Scripts/PlayerScripts/destroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("BallCol"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
