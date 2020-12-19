ba19a593e708bba444a4740f1fd98f96a9fa78d5
lifeandtimes89 <60407112+lifeandtimes89@users.noreply.github.com>
2020-12-19 23:47:19 +0000

    Added game collision sounds
    Added game collision sounds

--- Assets/3rdPerson+Fly/Scripts/PlayerScripts/destroy.cs
+++ Assets/3rdPerson+Fly/Scripts/PlayerScripts/destroy.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class destroy : MonoBehaviour
+  
 {
+    //public ParticleSystem explosionParticle;
+    public AudioClip bumpsound;
+    private AudioSource playerAudio;
 
+    private void Start()
+    {
+        playerAudio = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag.Equals("BallCol"))
         {
+            //explosionParticle.Play();
             Destroy(collision.gameObject);
+            playerAudio.PlayOneShot(bumpsound, 1.0f);
         }
     }
 }
