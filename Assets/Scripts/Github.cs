3dabe4537d50008d202c3ac416bd1f0bfc46f35c
lifeandtimes89 <60407112+lifeandtimes89@users.noreply.github.com>
2020-12-20 11:33:11 +0000

    Added UI instructions and github url button
    Added UI instructions and github url button

--- Assets/Scripts/Github.cs
+++ Assets/Scripts/Github.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Repository : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
-   public void OpenURL ()
+    void Update()
     {
-        Application.OpenURL("https://github.com/lifeandtimes89/IMMBeta");
-        Debug.Log("Hopefully its working");
+        
     }
 }
---
+++ Assets/Scripts/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repository : MonoBehaviour
+{
+    public string Url;
+    
+
+  
+   public void Open()
+    {
+        Application.OpenURL(Url);
+        Debug.Log("Hopefully its working");
+    }
+}
